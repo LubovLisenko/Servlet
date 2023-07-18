e0f7eb18b5262d1ba1e8c45b831cca7073c46cc5
LubovLisenko <lubovlisenko1985@gmail.com>
2023-07-18 16:22:43 +0300

    timezone fix

--- src/main/java/org/example/TimeServlet.java
+++ src/main/java/org/example/TimeServlet.java
@@ -10,33 +10,36 @@ import java.time.*;
 import java.time.format.DateTimeFormatter;
 
 
+
 @WebServlet(value = "/time")
 public class TimeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html; charset=UTF-8");
 
-       String timezone = req.getParameter("timezone");
+        String timezone = req.getParameter("timezone");
 
-        String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern(
-                "yyyy-MM-dd HH:mm:ss"
-        ));
 
-        String[] timeZoneSplit = timezone.split(" ");
-         String utc = timeZoneSplit[0];
-        int hour = Integer.parseInt(timeZoneSplit[1]);
+        if (req.getParameterMap().containsKey("timezone")){
+             req.getParameter("timezone");
+            String[] timeZoneSplit = timezone.split(" ");
+            String utc = timeZoneSplit[0];
+            int hour = Integer.parseInt(timeZoneSplit[1]);
+            resp.getWriter().write(LocalDateTime.now()
+                    .plusHours(hour - 3)
+                    .format(DateTimeFormatter.ofPattern(
+                            "yyyy-MM-dd HH:mm:ss")) + " " + utc + "+" + hour);
 
-        if (hour == 2){
-           resp.getWriter().write(currentTime + " UTC + 2");
+        } else {
 
-       } else resp.getWriter().write(LocalDateTime.now()
-                .plusHours(hour - 2)
+        resp.getWriter().write(String.valueOf(LocalDateTime.now().minusHours(3)
                 .format(DateTimeFormatter.ofPattern(
-                "yyyy-MM-dd HH:mm:ss")) + " " + utc + "+" + hour);
-        resp.getWriter().close();
-        super.doGet(req, resp);
+                "yyyy-MM-dd HH:mm:ss"))) + " UTC ");
     }
 
+        resp.getWriter().close();
+            super.doGet(req, resp);
+        }
 
     }
 
